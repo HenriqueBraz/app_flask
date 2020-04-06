f44e673195297b82577fab3c68b0ac9538fbb698
Felipe Keller Braz <fbraz3@gmail.com>
2020-04-05 23:12:50 -0300

    rodar pip install no deploy

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -8,5 +8,10 @@ cd /home/sites-python/novorumo.areadetestes.com.br && (
         chown -R novorumo.areadetestes.com.br:www-data LICENSE Procfile __pycache__ app config.json project.ini requirements.txt novorumo.sock;
         chown -R root. .git .gitignore .gitlab-ci.yml project.ini
         chmod a+r project.ini
-        chmod 700 $0
+        chmod 700 $0;
+)
+
+cd /home/sites-python/novorumo.areadetestes.com.br && (
+  source venv/bin/activate
+  for i in `cat requirements.txt`; do pip install $i; done
 )
