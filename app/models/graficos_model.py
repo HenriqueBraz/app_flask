49d536769a938bd15be13517ce31f098b98fd52f
Henrique <henrique.braz@acad.pucrs.br>
2020-06-15 02:05:44 -0300

    melhor do que antes

--- app/models/graficos_model.py
+++ app/models/graficos_model.py
@@ -32,11 +32,17 @@ class GraficoModel(object):
     def get_tributacao(self, tributacao1, tributacao2, tributacao3):
         result = []
         try:
-            self.cur.execute("SELECT COUNT(e.id) FROM empresas e  WHERE e.tributacao = '{}' AND e.status = 'Ativo';".format(tributacao1))
+            self.cur.execute(
+                "SELECT COUNT(e.id) FROM empresas e  WHERE e.tributacao = '{}' AND e.status = 'Ativo';".format(
+                    tributacao1))
             result += self.cur.fetchone()
-            self.cur.execute("SELECT COUNT(e.id) FROM empresas e  WHERE e.tributacao = '{}' AND e.status = 'Ativo';".format(tributacao2))
+            self.cur.execute(
+                "SELECT COUNT(e.id) FROM empresas e  WHERE e.tributacao = '{}' AND e.status = 'Ativo';".format(
+                    tributacao2))
             result += self.cur.fetchone()
-            self.cur.execute("SELECT COUNT(e.id) FROM empresas e  WHERE e.tributacao = '{}' AND e.status = 'Ativo';".format(tributacao3))
+            self.cur.execute(
+                "SELECT COUNT(e.id) FROM empresas e  WHERE e.tributacao = '{}' AND e.status = 'Ativo';".format(
+                    tributacao3))
             result += self.cur.fetchone()
             return result
         except Exception as e:
@@ -60,13 +66,9 @@ class GraficoModel(object):
         try:
             for i in range(1, 13):
                 self.cur.execute(
-<<<<<<< HEAD
-                "SELECT COUNT(c.tipo_cobranca ) FROM cobrancas c WHERE  DATE_FORMAT(c.data, '%m') = {} AND DATE_FORMAT(c.created, '%Y') = {} AND c.tipo_cobranca = '{}'".format(i, ano, tipo))
-=======
-                "SELECT COUNT(c.tipo_cobranca ) FROM cobrancas c WHERE  DATE_FORMAT(c.data, '%m') = {} AND DATE_FORMAT(c.created, '%Y') = {} AND c.tipo_cobranca = '{}' ".format(i, ano, tipo))
->>>>>>> front-end
+                    "SELECT COUNT(c.tipo_cobranca ) FROM cobrancas c WHERE  DATE_FORMAT(c.data, '%m') = {} AND DATE_FORMAT(c.created, '%Y') = {} AND c.tipo_cobranca = '{}' ".format(
+                        i, ano, tipo))
                 result += self.cur.fetchone()
-
 
             return result
         except Exception as e:
